5839405f710d4f1e498194d557c98c5d1d19d2c8
JCarvajalr <juandacr25@gmail.com>
2025-04-17 09:41:42 -0500

    1705A

--- 1705A.cpp
+++ 1705A.cpp
@@ -5,27 +5,16 @@
  
 using namespace std;
 void solve() {
-    int n, x, i, j = 0;
-    ll temp;
-    bool ans = true, sub;
+    int n, x, i;
+    bool ans = true;
     cin >> n  >> x;
     vector<ll> heights(n*2);
     for (i = 0; i < (n*2); i++) cin >> heights[i];
     
     sort(heights.begin(), heights.end());
     
-    for (i = 0; (ans) && (i < n); i++) {
-        temp = heights[i];
-        if (temp < 0) continue;
-        sub = false;
-        for (j = i+1; j < heights.size(); j++) {
-            if ((heights[j] - temp) >= x) {
-                heights[j] = -1;
-                sub = true;
-                break;
-            }
-        }
-        ans = sub;
+    for (i = 0; ans && (i < n); i++) {
+        ans = (heights[n + i] - heights[i] >= x);
     }
     cout << ((ans) ? "YES\n" : "NO\n");
 }
@@ -34,8 +23,7 @@ int main() {
     fastread();
     int test;
     cin >> test;
-    while (test--) {
-        solve();
-    }
+    while (test--) solve();
+    
     return 0;
 }
