d55215b90e21c651861fcc029177d10a330d6c80
JCarvajalr <juandacr25@gmail.com>
2025-07-31 16:38:25 -0500

    template

--- Template.cpp
+++ Template.cpp
@@ -3,12 +3,16 @@
 #define fastread() (ios_base:: sync_with_stdio(false),cin.tie(NULL));
 #define ll long long
 #define _for(n) for (i = 0; i < n; i++)
+#define _forj(n) for (j = 0; j < n; j++)
 #define deb(x) cout << " ." << #x << "=" << x << endl
 #define deb2(a, b) cout << " /:" << #a << "=" << a << " , " << #b << "=" << b << endl
- 
+#define sortAsc(vect) sort(vect.begin(), vect.end())
+#define sortDesc(vect) sort(vect.begin(), vect.end(), greater<int>())
+
 using namespace std;
 void solve() {
     int n, i, temp;
+
 }
 
 int main() {
