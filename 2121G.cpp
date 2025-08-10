b136d6879c7bb28885594bffa883c5788f949021
JCarvajalr <juandacr25@gmail.com>
2025-08-09 19:02:57 -0500

    2121G

---
+++ 2121G.cpp
@@ -0,0 +1,48 @@
+// G. Gangsta
+#include <bits/stdc++.h>
+#define fastread() (ios_base:: sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL))
+#define ll long long
+#define _for(n) for (i = 0; i < n; i++)
+#define _forj(n) for (j = 0; j < n; j++)
+#define deb(x) cout << " ." << #x << "=" << x << endl
+#define deb2(a, b) cout << " /:" << #a << "=" << a << " , " << #b << "=" << b << endl
+#define sortAsc(vect) sort(vect.begin(), vect.end())
+#define sortDesc(vect) sort(vect.begin(), vect.end(), greater<int>())
+
+using namespace std;
+void solve() {
+    int n;
+    cin >> n;
+    ll zeros, ones, add, total, cur = n;
+    zeros = ones = add = total = 0;
+    vector<ll> ar((n*2)+1, 0);
+    char c;
+    for (int i = 0; i < n; i++) {
+        cin >> c;
+        if (c == '1') {
+            ar[cur]++;
+            ones += ar[cur];
+            cur++;
+            zeros -= ar[cur];
+            add += ones;
+        } else {
+            ar[cur]++;
+            zeros += ar[cur];
+            cur--;
+            ones -= ar[cur];
+            add += zeros;
+        }
+        total += add;
+    }
+    cout << total << endl;
+}
+
+int main() {
+    fastread();
+    int test = 1;
+    cin >> test;
+    while (test--){
+        solve();
+    }
+    return 0;
+}
