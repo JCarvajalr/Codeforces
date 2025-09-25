3ce001b928e060fa4e9d5fcb7daf0516675ec623
JCarvajalr <juandacr25@gmail.com>
2025-09-24 19:06:03 -0500

    update Template

--- Template.cpp
+++ Template.cpp
@@ -1,25 +1,26 @@
 // 
 #include <bits/stdc++.h>
-#define fastread() (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+#define fastread() (ios_base:: sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL))
 #define ll long long
 #define _for(n) for (i = 0; i < n; i++)
 #define _forj(n) for (j = 0; j < n; j++)
 #define deb(x) cout << " ." << #x << "=" << x << endl
-#define deb2(a, b) cout << " /:" << #a << "=" << a << " , " << #b << "=" << b << endl
+#define deb2(a, b) cout << "  /:" << #a << "=" << a << " , " << #b << "=" << b << endl
+#define debArr(array) cout << "  /:" << #array << "[] = "; for (int I = 0; I < (int) array.size(); I++) cout << array[I] << " "; cout << endl;
 #define sortAsc(vect) sort(vect.begin(), vect.end())
 #define sortDesc(vect) sort(vect.begin(), vect.end(), greater<int>())
+// int maxInt = INT_MAX;
 
 using namespace std;
 void solve() {
     int n, i, temp;
-
+    
 }
 
 int main() {
+    fastread();
     int test;
     cin >> test;
-    while (test--){
-        solve();
-    }
+    while (test--) solve();
     return 0;
 }
